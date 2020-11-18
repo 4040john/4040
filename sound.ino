f230ec6b62e088a91e95efb7544f9441182598f2
4040john <4040john@naver.com>
2020-11-18 10:07:36 +0900

    v1

---
+++ sound.ino
@@ -0,0 +1,20 @@
+#define SOUND 8 
+
+void setup() {
+
+  Serial.begin(9600);
+  
+  
+}
+
+void loop() {
+  if (analogRead(A0)>600)
+  {
+      Serial.println("hello");
+  }
+  Serial.println(analogRead(A0));
+  delay(50);
+
+
+
+}
